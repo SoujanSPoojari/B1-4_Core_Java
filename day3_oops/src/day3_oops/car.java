b3e2d307deba824d3503c6822d775d6787f3ae82
HP <HP@LAPTOP-7DRK1N1V>
2022-03-30 18:59:54 +0530

    First Commit

---
+++ day3_oops/src/day3_oops/car.java
@@ -0,0 +1,16 @@
+package day3_oops;
+
+public class car {
+
+	
+		// TODO Auto-generated method stub
+		void display()
+		{
+			System.out.println("Welcome to c2tc");
+		}
+		public static void main(String[] args) {
+			car c= new car();
+			c.display();
+	}
+
+}
---
+++ day3_oops/src/day3_oops/example1.java
@@ -0,0 +1,10 @@
+package day3_oops;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
