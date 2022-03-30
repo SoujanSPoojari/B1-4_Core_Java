929e84aba75fef547cd6e3d45f5d05a8460aca50
HP <HP@LAPTOP-7DRK1N1V>
2022-03-30 18:38:49 +0530

    First Commit

---
+++ day2_introtojava/src/day2_introtojava/example3.java
@@ -0,0 +1,21 @@
+package day2_introtojava;
+
+public class example3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=6,b=12;
+		boolean s1=true;
+		boolean s2=false;
+		if(a>b)
+		{
+			System.out.println(s1);
+		}
+		else
+		{
+			System.out.println(s2);
+		}
+	}
+	
+
+}
