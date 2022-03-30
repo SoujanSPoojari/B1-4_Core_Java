d1bc521355df12a4ed7e9f04bbc6d425cdb0e38d
HP <HP@LAPTOP-7DRK1N1V>
2022-03-30 16:12:45 +0530

    First Commit

--- day2_introtojava/src/day2_introtojava/factorial.java
+++ day2_introtojava/src/day2_introtojava/factorial.java
@@ -1,10 +1,21 @@
 package day2_introtojava;
 
+import java.util.Scanner;
+
 public class factorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int fact=1;
+		for(int i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		
+		}
+		System.out.println(fact);
+		s.close();
 	}
 
 }
