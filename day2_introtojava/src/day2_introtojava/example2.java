65edb17a5a0935a1053e974fb82bb5359956bd69
HP <HP@LAPTOP-7DRK1N1V>
2022-03-30 18:33:17 +0530

    First Commit

---
+++ day2_introtojava/src/day2_introtojava/example2.java
@@ -0,0 +1,20 @@
+package day2_introtojava;
+
+import java.util.Scanner;
+
+public class example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s =new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		s.close();
+	}
+
+}
---
+++ day2_introtojava/src/day2_introtojava/introduction.java
@@ -0,0 +1,14 @@
+package day2_introtojava;
+import java.util.Scanner;
+
+public class introduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s =new Scanner(System.in);
+		char c=s.next().charAt(5);
+		System.out.println(c);
+		s.close();
+	}
+
+}
