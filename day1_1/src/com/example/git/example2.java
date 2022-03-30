4f204df3eaecd43e58e12ae2ed3c2fe64e93f68f
HP <HP@LAPTOP-7DRK1N1V>
2022-03-30 15:52:08 +0530

    First Commit

--- day1_1/src/com/example/git/example2.java
+++ day1_1/src/com/example/git/example2.java
@@ -4,7 +4,21 @@ public class example2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 // declare variables
+	    int a = 12, b = 5;
+	    System.out.println("a + b = " + (a + b));
 
+	   
+	    System.out.println("a - b = " + (a - b));
+
+	    
+	    System.out.println("a * b = " + (a * b));
+
+	    
+	    System.out.println("a / b = " + (a / b));
+
+	   
+	    System.out.println("a % b = " + (a % b));
 	}
 
 }
