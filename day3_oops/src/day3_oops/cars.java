8a0c1ce50bcc80f447668d0f42f990eb8d766b8e
HP <HP@LAPTOP-7DRK1N1V>
2022-03-31 17:15:34 +0530

    First Commit

--- day3_oops/src/day3_oops/cars.java
+++ day3_oops/src/day3_oops/cars.java
@@ -1,5 +1,4 @@
 package day3_oops;
-
 public class cars {
 	String str;
 	int a;
@@ -13,11 +12,11 @@ public class cars {
 		System.out.println(str+" "+a);
 	}
 	public static void main(String[] args) {
+
 		
-		
-		cars c=new cars("Welcome to Audi", 0, 0, 0);
-		cars c1=new cars("Welcome to BMW", 0, 0, 0);
-		cars c2=new cars("Welcome to Benz", 0, 0, 0);
+		cars c=new cars("This is Audi", 0, 0, 0);
+		cars c1=new cars("This is BMW", 0, 0, 0);
+		cars c2=new cars("This is Benz", 0, 0, 0);
 		c.display();
 		c1.display();
 		c2.display();
