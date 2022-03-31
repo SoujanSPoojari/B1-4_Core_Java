01ada1ebb7d2fbda8562a4e4824ed3003d3849f5
HP <HP@LAPTOP-7DRK1N1V>
2022-03-31 19:11:51 +0530

    First Commit

---
+++ day4_packages/src/com/package3/sbi.java
@@ -0,0 +1,13 @@
+package com.package3;
+
+public class sbi {
+	public int balance;
+	public sbi(int bal)
+	{
+		balance=bal;
+	}
+	public void display()
+	{
+		System.out.print(balance);
+	}
+}
---
+++ day4_packages/src/com/package4/encapsulation.java
@@ -0,0 +1,12 @@
+package com.package4;
+import com.package3.sbi;
+
+public class encapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sbi sb=new sbi(25000);
+		sb.display();
+	}
+
+}
