5d50d9914b4cd8876433c3d49b1d39d53f596383
HP <HP@LAPTOP-7DRK1N1V>
2022-03-31 18:13:27 +0530

    First Commit

---
+++ day4_packages/src/com/package2/example2.java
@@ -0,0 +1,11 @@
+package com.package2;
+import day4_packages.*;
+public class example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		example1 e=new example1();
+		e.display();
+	}
+
+}
---
+++ day4_packages/src/day4_packages/example1.java
@@ -0,0 +1,8 @@
+package day4_packages;
+
+public class example1 {
+	public void display()
+	{
+		System.out.println("Welcome to tns");
+	}
+}
