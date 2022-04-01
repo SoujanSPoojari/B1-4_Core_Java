28214798ca63f8eaf1c5daa5714e38b086490c90
HP <HP@LAPTOP-7DRK1N1V>
2022-04-01 13:49:04 +0530

    First Commit

---
+++ day4_packages/src/com/package5/sbi_assignment.java
@@ -0,0 +1,16 @@
+package com.package5;
+
+public class sbi_assignment {
+	public int balance;
+	public sbi_assignment() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getBalance()
+	{
+		return balance;
+	}
+	public void setBalance(int fal, int balance)
+	{
+		this.balance=balance;
+	}
+}
---
+++ day4_packages/src/com/package6/encapsulation_assignment.java
@@ -0,0 +1,10 @@
+package com.package6;
+import com.package5.sbi_assignment;
+public class encapsulation_assignment {
+	public static void main(String[] args)
+	{
+		sbi_assignment sb=new sbi_assignment();
+		sb.setBalance(0,32500);
+		System.out.print("your SB A/c Balance:"+sb.getBalance());
+	}
+}
