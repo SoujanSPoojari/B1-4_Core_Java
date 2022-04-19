591d457737909391ba6553249d300826c38d0711
HP <HP@LAPTOP-7DRK1N1V>
2022-04-19 18:22:47 +0530

    First Commit

---
+++ Assignment_3_Tree_Set_Map/src/com/collection/Example26.java
@@ -0,0 +1,16 @@
+package com.collection;
+
+import java.util.ArrayDeque;
+
+public class Example26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayDeque a = new ArrayDeque();
+		System.out.println("Soujan");
+		a.addFirst("S");
+		a.addLast("P");
+		System.out.println(a);
+	}
+
+}
---
+++ Assignment_3_Tree_Set_Map/src/com/collection/Example27.java
@@ -0,0 +1,24 @@
+package com.collection;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Example27 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SortedSet s= new TreeSet();
+		s.add(15);
+		s.add(35);
+		s.add(25);
+		s.add(55);
+		s.add(45);
+		s.add(75);
+		s.add(65);
+		s.add(85);
+		s.add(95);
+		s.add(105);
+		System.out.print(s);
+	}
+
+}
