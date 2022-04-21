5f03e543b913c1ea2ff0e68c8e9d7ff423b7b568
HP <HP@LAPTOP-7DRK1N1V>
2022-04-21 23:16:35 +0530

    First Commit

--- Day_Assignment4_Recursion/src/com/recursion/Example2_1.java
+++ Day_Assignment4_Recursion/src/com/recursion/Example2_1.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Example2_1 {
 
-	 static int numberOfPaths(int m, int n)
+	 static int p(int m, int n)
 	    {
 	        if (m == 1 || n == 1)
 	            return 1;
-	        return numberOfPaths(m - 1, n) + numberOfPaths(m, n - 1);
+	        return p(m - 1, n) + p(m, n - 1);
 	    }
 	    public static void main(String args[])
 	    {
 	    	Scanner s= new Scanner(System.in);
 			int n= s.nextInt();
 			int m=s.nextInt();
-			int a=numberOfPaths(n,m);
+			int a=p(n,m);
 			System.out.print(a);
 			s.close();
 
