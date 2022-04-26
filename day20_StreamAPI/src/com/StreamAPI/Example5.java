09d0c7cfa04da05114b889bac92021fef23da24e
HP <HP@LAPTOP-7DRK1N1V>
2022-04-26 23:16:13 +0530

    First Commit

--- day20_StreamAPI/src/com/StreamAPI/Example5.java
+++ day20_StreamAPI/src/com/StreamAPI/Example5.java
@@ -1,5 +1,6 @@
 package com.StreamAPI;
 
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
@@ -13,12 +14,28 @@ public class Example5 {
 	void display() {
 		// TODO Auto-generated method stub
 		
-		List<String>values=Arrays.asList("Soujan","Soujan","Soujan","Soujan","Soujan");
-		//Optional<String>var=values.stream().findAny();
+		//List<String>values=Arrays.asList("Soujan","Soujan","Soujan","Soujan","Soujan");
+		//Optional<String>var=values.stream().parallel().filter().findAny();
 		//assertTrue(var.isPresent());
-		//assertThat(var.get());
-		values.stream().findAny().equals(values);
+		//assertThat(var.get(),anyOf(is("Soujan")));
+		//values.stream().findAny().equals(values);
+		List<String> values = Arrays.asList("A", "B", "C", "D");
+
+	    Optional<String> var = values.stream().findFirst();
+
+	    assertTrue(var.isPresent());
+	    assertThat(var.get(), is("A"));
 		System.out.println(values);
 	
+	}
+
+	private void assertThat(String string, Object object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private Object is(String string) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
---
+++ day21_Annotation/src/com/annotation/Example6.java
@@ -0,0 +1,10 @@
+package com.annotation;
+import java.lang.annotation.Documented;
+@Documented @interface foodie{}
+public class Example6 {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Folks");
+	}
+}
+
