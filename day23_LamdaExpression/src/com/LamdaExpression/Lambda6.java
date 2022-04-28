47c7011d15ac04785734a296e40f74ad538d2b79
HP <HP@LAPTOP-7DRK1N1V>
2022-04-28 18:37:50 +0530

    First Commit

---
+++ day23_LamdaExpression/src/com/LamdaExpression/Lambda6.java
@@ -0,0 +1,15 @@
+package com.LamdaExpression;
+interface D
+{
+	int mul(int x,int y);
+}
+
+public class Lambda6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		D obj=(x,y)->(x*y);
+		System.out.println(obj.mul(20, 30));
+	}
+
+}
