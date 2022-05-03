260bd2557c8d94558110e1b5a19aef2ff80f60e3
HP <HP@LAPTOP-7DRK1N1V>
2022-05-03 19:01:37 +0530

    First Commit

---
+++ day24_Java_Enum_and_REGEX/src/com/Java_Enum_and_REGEX/Example1.java
@@ -0,0 +1,14 @@
+package com.Java_Enum_and_REGEX;
+enum Car
+{
+	Mercedes,BMW,Audi
+}
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car c=Car.BMW;
+		System.out.println(c);
+	}
+
+}
